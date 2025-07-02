946d94b806c64dce75244517058e61c77ad45092
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-07-02 11:40:58 +0200

    Milestone 3 solved

---
+++ src/components/TaskRow.jsx
@@ -0,0 +1,15 @@
+const TaskRow = function({task}) {
+
+    const statusClass = task.status.replace(" ","").toLowerCase();
+
+    return (
+        <tr>
+            <td>{task.title}</td>
+            <td className={statusClass}>{task.status}</td>
+            <td>{new Date(task.createdAt).toLocaleDateString()}</td>
+        </tr>
+    )
+
+}
+
+export default TaskRow;
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,13 +1,26 @@
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
+import TaskRow from "../components/TaskRow";
 
 export default function TaskList() {
     
     const { tasks } = useContext(GlobalContext);
-  
+    console.log("Tasks:", tasks);
+    
     return (
-        <div className="task-list">
-           Lista Task
-        </div>    
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Stato</th>
+                    <th>Data di creazione</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks.map((task) => (
+                    <TaskRow key={task.id} task={task} />
+                ))}
+            </tbody>
+        </table>    
     );
 }

b964f99055c44a45f68e048630b0150552978bd9
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-07-02 16:42:57 +0200

    Milestonew 8 solved

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,11 +1,12 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import { useContext} from "react"
 import { GlobalContext } from "../context/GlobalContext"
 
 export default function TaskDetails() {
 
     const { id } = useParams()
-    const { tasks } = useContext(GlobalContext)
+    const navigate = useNavigate()
+    const { tasks, removeTask } = useContext(GlobalContext)
 
     const task = tasks.find(task => task.id === parseInt(id))
 
@@ -13,14 +14,21 @@ export default function TaskDetails() {
         return <h1>Task not found</h1>        
     }
     
-    const handleDelete = () => {
-        console.log('Eliminazione task con ID:', task.id);
+    const handleDelete = async () => {
+        try {
+            await removeTask(task.id)
+            alert("Task eliminata con successo")
+            navigate('/')
+        } catch (error) {
+            console.error("Error deleting task:", error)
+            alert("Errore durante l'eliminazione della task")
+        }
     }
 
     return (
         <div className="task-details">
             <h2>Task</h2>
-            <p><strong>Nome:</strong> {task.title}</p>
+            <p><strong>Nome:</strong> {task.title}</p>3
             <p><strong>Descrizione:</strong> {task.description}</p>
             <p><strong>Stato:</strong> {task.status}</p>
             <p><strong>Data di creazione:</strong> {new Date(task.createdAt).toLocaleDateString()}</p>
